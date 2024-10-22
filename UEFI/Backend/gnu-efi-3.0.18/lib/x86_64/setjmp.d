436f70e50a2d10b73bb7598f2032eb9276553c9c
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-10-22 21:02:53 +0300

    UEFI source code

---
+++ UEFI/Backend/gnu-efi-3.0.18/lib/x86_64/setjmp.d
@@ -0,0 +1,2 @@
+C:\Users\victim\Documents\GitHub\HaikuRansomware\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\setjmp.o: \
+ C:\Users\victim\Documents\GitHub\HaikuRansomware\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\setjmp.S
